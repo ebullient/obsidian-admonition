96b7e6055a2477355e3926c9b925ff1e714b76b5
Erin Schnabel <ebullientworks@gmail.com>
2026-09-24 09:45:41 -0400

    🐛 avoid hierarchy error w/ suggester; resolves #440

--- src/suggest/suggest.ts
+++ src/suggest/suggest.ts
@@ -50,7 +50,7 @@ abstract class AdmonitionOrCalloutSuggester extends EditorSuggest<
                 iconEl.removeClass("svg-inline--fa");
                 iconEl.addClass("svg-icon");
             }
-            iconDiv.appendChild(iconEl ?? activeDocument.createDiv());
+            iconDiv.appendChild(iconEl ?? createDiv());
         }
     }
     onTrigger(
